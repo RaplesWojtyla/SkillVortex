cc090c2dec6cd39d02ecd738cafccc367e2b80ab
Wojtyla <rapleswojtyla@gmail.com>
2023-11-19 18:52:43 +0700

    Kelupaan 'require_once()':(

--- authentication/sendMail.php
+++ authentication/sendMail.php
@@ -1,6 +1,8 @@
 <?php
 
+    require_once('./includes/koneksi.php');
     require('../includes/function.php');
+    
     $code = generateCode();
     $_SESSION['verifCode'] = $code;
     $email = $_POST['email'];
