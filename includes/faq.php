7d8f4e06552239ad34d5e7802b0e9ef8ca0f0cbe
GoatMudryk <alfioke7@gmail.com>
2023-12-23 23:59:20 +0700

    FAQ

---
+++ includes/faq.php
@@ -0,0 +1,128 @@
+                                                <tr>
+                                                    <td>
+                                                        <h3 class="text-center">FAQ</h3>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <div class="accordion accordion-flush" id="accordionPanelsStayOpenExample">
+                                                            <!-- Table Materi -->
+                                                            <div class="accordion-item">
+                                                                <h6 class="accordion-header">
+                                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq4" aria-expanded="true" aria-controls="panelsStayOpen-collapseTwo">
+                                                                        <h5>Apa itu FAQ dan mengapa saya perlu membacanya?</h5>
+                                                                    </button>
+                                                                </h6>
+                                                                <div id="faq4" class="accordion-collapse collapse" data-bs-parent="#accordionPanelsStayOpenExample">
+                                                                    <div class="accordion-body">
+                                                                        FAQ adalah singkatan dari "Frequently Asked Questions" atau "Pertanyaan yang Sering Diajukan." 
+                                                                        Ini adalah kumpulan pertanyaan-pertanyaan yang sering diajukan oleh pengguna atau pelanggan, 
+                                                                        beserta jawabannya. FAQ dirancang untuk memberikan informasi cepat dan langsung kepada 
+                                                                        pengguna mengenai topik atau layanan tertentu.
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="accordion-item">
+                                                                <h6 class="accordion-header">
+                                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq5" aria-expanded="true" aria-controls="panelsStayOpen-collapseTwo">
+                                                                        <h5>Apa itu Skill Vortex?</h5>
+                                                                    </button>
+                                                                </h6>
+                                                                <div id="faq5" class="accordion-collapse collapse" data-bs-parent="#accordionPanelsStayOpenExample">
+                                                                    <div class="accordion-body" >
+                                                                        Skill Vortex adalah suatu sistem pembelajaran online yang memungkinkan pengguna 
+                                                                        untuk mengakses kursus, materi pembelajaran, dan sumber daya pendidikan secara daring.
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="accordion-item">
+                                                                <h6 class="accordion-header">
+                                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq7" aria-expanded="true" aria-controls="panelsStayOpen-collapseTwo">
+                                                                        <h5>Bagaimana cara memulai course?</h5>
+                                                                    </button>
+                                                                </h6>
+                                                                <div id="faq7" class="accordion-collapse collapse" data-bs-parent="#accordionPanelsStayOpenExample">
+                                                                    <div class="accordion-body">
+                                                                        <ol>
+                                                                            <li>Setelah login, pilih kursus yang ingin diambil dari menu course.</li>
+                                                                            <li>Klik "Enroll" untuk mengambil course</li>
+                                                                            <li>pindah ke menu dashboard</li>
+                                                                            <li>klik "course" di card course yang ingin dimasuki untuk mengakses materi pembelajaran</li>
+                                                                        </ol>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="accordion-item">
+                                                                <h6 class="accordion-header">
+                                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq6" aria-expanded="true" aria-controls="panelsStayOpen-collapseTwo">
+                                                                        <h5>Apakah konten course dapat diakses secara offline?</h5>
+                                                                    </button>
+                                                                </h6>
+                                                                <div id="faq6" class="accordion-collapse collapse" data-bs-parent="#accordionPanelsStayOpenExample">
+                                                                    <div class="accordion-body">
+                                                                        Saat ini, konten kursus hanya dapat diakses secara daring, namun, 
+                                                                        beberapa materi dapat diunduh untuk diakses secara offline sesuai kebijakan pengajar.
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="accordion-item">
+                                                                <h6 class="accordion-header">
+                                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq3" aria-expanded="true" aria-controls="panelsStayOpen-collapseTwo">
+                                                                        <h5>Apa yang harus dilakukan jika mengalami masalah teknis?</h5>
+                                                                    </button>
+                                                                </h6>
+                                                                <div id="faq3" class="accordion-collapse collapse" data-bs-parent="#accordionPanelsStayOpenExample">
+                                                                    <div class="accordion-body">
+                                                                        <ol>
+                                                                            <li>Pastikan koneksi internet Anda stabil. Restart modem atau router Anda jika diperlukan.</li>
+                                                                            <li>Jika masalah terjadi pada situs web, coba muat ulang halaman tersebut atau gunakan tombol refresh pada browser.</li>
+                                                                            <li>Pastikan perangkat keras seperti komputer atau ponsel berfungsi dengan baik. Restart perangkat jika diperlukan.</li>
+                                                                            <li>Lihat apakah ada pesan galat atau pemberitahuan khusus. Pesan ini sering memberikan petunjuk tentang masalah yang terjadi.</li>
+                                                                            <li>Jika langkah-langkah di atas tidak memperbaiki masalah, silakan hubungi tim dukungan teknis kami yang ada di atas</li>
+                                                                        </ol>  
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="accordion-item">
+                                                                <h6 class="accordion-header">
+                                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq2" aria-expanded="true" aria-controls="panelsStayOpen-collapseTwo">
+                                                                        <h5>Bagaimana cara mengganti username atau kata sandi saya?</h5>
+                                                                    </button>
+                                                                </h6>
+                                                                <div id="faq2" class="accordion-collapse collapse" data-bs-parent="#accordionPanelsStayOpenExample">
+                                                                    <div class="accordion-body">
+                                                                        <ol>
+                                                                            <li>Klik Profil yang ada di pojok kanan atas</li>
+                                                                            <li>Klik My Account untuk mengganti username</li>
+                                                                            <li>Klik Change Password untuk mengganti kata sandi</li>
+                                                                            <li>Akan muncul tampilan untuk mengganti username atau kata sandi, gantilah sesuai yang diinginkan</li>
+                                                                            <li>Klik Change untuk menyimpan perubahan dan close untuk membatalkan perubahan</li>
+                                                                        </ol>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="accordion-item">
+                                                                <h6 class="accordion-header">
+                                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq1" aria-expanded="true" aria-controls="panelsStayOpen-collapseTwo">
+                                                                        <h5>Apa yang harus dilakukan jika lupa kata sandi?</h5>
+                                                                    </button>
+                                                                </h6>
+                                                                <div id="faq1" class="accordion-collapse collapse" data-bs-parent="#accordionPanelsStayOpenExample">
+                                                                    <div class="accordion-body">
+                                                                        <ol>
+                                                                            <li>Masuk ke menu Login</li>
+                                                                            <li>Scroll ke bawah dan klik <a href="../authentication/forgot_password.php">Forgot Password</a></li>
+                                                                            <li>Masukkan Email akun anda</li>
+                                                                            <li>klik send code dan masukkan kode yang masuk ke email anda</li>
+                                                                            <li>klik check</li>
+                                                                            <li>Ubah ulang kata sandi Akun Anda</li>
+                                                                            <li>Silakan Login Kembali</li>
+                                                                        </ol>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                               
+                                                
